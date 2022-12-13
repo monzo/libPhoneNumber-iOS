f3fa1006aa73a83e65ae397d39ecade45556d007
Tim Mikelj <timmikelj@monzo.com>
2022-12-13 18:14:57 +0000

    revert to support all compatible platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,11 @@ import PackageDescription
 let package = Package(
     name: "libPhoneNumber",
     platforms: [
+        .macOS(.v10_10),
+        .macCatalyst(.v13),
         .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
     ],
     products: [
         .library(
