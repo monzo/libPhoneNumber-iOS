3afb733e24ea369a9f5b5c17965370a0b0fcac3f
Tim Mikelj <88086514+timmikeljmonzo@users.noreply.github.com>
2022-12-14 11:05:39 +0000

    📦 Update `Package.swift` & remove cocoapod support (#1)
    * update Package.swift
    
    * default to SPM, update readme + remove podspec files
    
    * revert to support all compatible platforms

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "libPhoneNumber",
+    platforms: [
+        .macOS(.v10_10),
+        .macCatalyst(.v13),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         .library(
             name: "libPhoneNumber",
@@ -18,19 +25,9 @@ let package = Package(
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath("Internal")
-            ]
-        ),
-        .testTarget(
-            name: "libPhoneNumberTests",
-            dependencies: ["libPhoneNumber"],
-            path: "libPhoneNumberTests",
-            sources: [
-                "NBAsYouTypeFormatterTest.m",
-                "NBPhoneNumberParsingPerfTest.m",
-                "NBPhoneNumberUtil+ShortNumberTestHelper.h",
-                "NBPhoneNumberUtil+ShortNumberTestHelper.m",
-                "NBPhoneNumberUtilTest.m",
-                "NBShortNumberInfoTest.m"
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreTelephony", .when(platforms: [.iOS, .macOS, .macCatalyst])),
             ]
         )
     ]
